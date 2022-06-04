1b309be095f11e8d1742e863a591449a6475f39e
Bridgetxyz <106855750+Bridgetxyz@users.noreply.github.com>
2022-06-04 16:42:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const CONFIG = {
     btnIntro: '^^HiHi^^',
     title: 'có thích t khum',
     desc: 'chọn điiiiiii',
-    btnYes: 'không nha',
+    btnYes: 'có nhaaa',
     btnNo: 'thích lắm',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'hay khummm',
     btnAccept: 'cóooooooo',
-    messLink: ' https://www.facebook.com/messages/t/100066283205492' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: ' https://www.facebook.com/messages/t/100059038294532 //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
